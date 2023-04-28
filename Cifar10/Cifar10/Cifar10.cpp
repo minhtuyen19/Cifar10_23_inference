099a0a157fbc31ce1a7d87745d4ff80ed18ff331
minhtuyen2305 <81373214+minhtuyen2305@users.noreply.github.com>
2023-04-28 16:09:53 +0700

    fix

--- Cifar10/Cifar10/Cifar10.cpp
+++ Cifar10/Cifar10/Cifar10.cpp
@@ -38,11 +38,11 @@ void conv2d(float* input, float* w, int row, int col, int frow, int fcol, int ch
     int out_col = col - fcol + 1;
     int s = 0;
 
-    // Allocate memory for kernel_out
+    // Allocate dynamically memory for kernel_out from w
     float**** kernel_out = new float*** [numofkernel];
     for (int k = 0; k < numofkernel; k++) {
         kernel_out[k] = new float** [channel_inputs];
-        for (int l = 0; l < channel_inputs; l++) { // fixed typo in loop condition
+        for (int l = 0; l < channel_inputs; l++) { 
             kernel_out[k][l] = new float* [frow];
             for (int i = 0; i < frow; i++) {
                 kernel_out[k][l][i] = new float[fcol];
@@ -53,7 +53,7 @@ void conv2d(float* input, float* w, int row, int col, int frow, int fcol, int ch
             }
         }
     }
-
+    //thuc hien nhan chap inpu vs moi kernel set
     for (int k = 0; k < numofkernel; k++) {
         for (int i = 0; i < out_row; i++) {
             for (int j = 0; j < out_col; j++) {
@@ -84,118 +84,6 @@ void conv2d(float* input, float* w, int row, int col, int frow, int fcol, int ch
     }
     delete[] kernel_out;
 }
-
-//void conv2d(float* input, float* w, int row, int col, int frow, int fcol, int n_input, int numofkernel, float* conv_results, float* bias, int padding) {
-//    int channel_inputs = n_input;
-//    int out_row =  row - frow + 1 + 2 * padding;
-//    int out_col = col - fcol + 1 + 2 * padding;
-//    // Allocate memory for kernel_out
-//    float**** kernel_out = new float*** [channel_inputs];
-//    for (int l = 0; l < channel_inputs; l++) {
-//        kernel_out[l] = new float** [numofkernel];
-//        for (int k = 0; k < numofkernel; k++) {
-//            kernel_out[l][k] = new float* [frow];
-//            for (int i = 0; i < frow; i++) {
-//                kernel_out[l][k][i] = new float[fcol];
-//                for (int j = 0; j < fcol; j++) {
-//                    int kernel_idx = l * numofkernel * frow * fcol + k * frow * fcol + i * fcol + j;
-//                    kernel_out[l][k][i][j] = w[kernel_idx];
-//                }
-//            }
-//        }
-//    }
-//
-//    for (int k = 0; k < numofkernel; k++) {
-//        for (int i = -padding; i < out_row - padding; i++) {
-//            for (int j = -padding; j < out_col - padding; j++) {
-//                float sum = 0;
-//                for (int l = 0; l < channel_inputs; l++) {
-//                    for (int m = 0; m < frow; m++) {
-//                        for (int n = 0; n < fcol; n++) {
-//                            int input_idx = l * row * col + (i + m) * col + j + n;
-//                            int kernel_idx = l * numofkernel * frow * fcol + k * frow * fcol + m * fcol + n;
-//                            if (i + m >= 0 && i + m < row && j + n >= 0 && j + n < col) {
-//                                sum += input[input_idx] * kernel_out[l][k][m][n];
-//                            }
-//                        }
-//                    }
-//                }
-//                int h = k * out_row * out_col + (i + padding) * out_col + j + padding;
-//                conv_results[k * out_row * out_col + (i + padding) * out_col + j + padding] = sum + bias[k];
-//            }
-//        }
-//    }
-//    //for (int i = 0; i < (row - frow + 1) * (col - fcol + 1) * numofkernel; i++) {
-//    //  printf("out_conv2d[%d] = %0.10f ", i, conv_results[i]);
-//    //}
-//    // Free memory for kernel_out
-//    for (int l = 0; l < channel_inputs; l++) {
-//        for (int k = 0; k < numofkernel; k++) {
-//            for (int i = 0; i < frow; i++) {
-//                delete[] kernel_out[l][k][i];
-//            }
-//            delete[] kernel_out[l][k];
-//        }
-//        delete[] kernel_out[l];
-//    }
-//    delete[] kernel_out;
-//}
-
-//void conv2d(float* input, float* w, int row, int col, int frow, int fcol, int n_input, int numofkernel, float* conv_results, float* bias) {
-//    int channel_inputs = n_input;
-//    int out_row = row - frow + 1;
-//    int out_col = col - fcol + 1;
-//    int s = 0;
-//    // Allocate memory for kernel_out
-//    float**** kernel_out = new float*** [channel_inputs];
-//    for (int l = 0; l < channel_inputs; l++) {
-//        kernel_out[l] = new float** [numofkernel];
-//        for (int k = 0; k < numofkernel; k++) {
-//            kernel_out[l][k] = new float* [frow];
-//            for (int i = 0; i < frow; i++) {
-//                kernel_out[l][k][i] = new float[fcol];
-//                for (int j = 0; j < fcol; j++) {
-//                    kernel_out[l][k][i][j] = w[s++];
-//                }
-//            }
-//        }
-//    }
-//
-//    for (int k = 0; k < numofkernel; k++) {
-//        for (int i = 0; i < out_row; i++) {
-//            for (int j = 0; j < out_col; j++) {
-//                float sum = 0;
-//                for (int l = 0; l < channel_inputs; l++) {
-//                    for (int m = 0; m < frow; m++) {
-//                        for (int n = 0; n < fcol; n++) {
-//                            int input_idx = l * row * col + (i + m) * col + j + n;
-//                            int kernel_idx = l * channel_inputs * frow * fcol + k * frow * fcol + m * fcol + n;
-//                                 sum += input[input_idx] * kernel_out[l][k][m][n];
-//                            //printf("kernel_out[%d][%d][%d][%d] = %0.10f ", l, k, m, n, kernel_out[l][k][m][n]);
-//                        }
-//                    }
-//                }
-//                int h = k * out_row * out_col + i * out_col + j;
-//                conv_results[k * out_row * out_col + i * out_col + j] = sum + bias[k];
-//            }
-//        }
-//    }
-//    //for (int i = 0; i < (row - frow + 1) * (col - fcol + 1) * numofkernel; i++) {
-//    //  printf("out_conv2d[%d] = %0.10f ", i, conv_results[i]);
-//    //}
-//    // Free memory for kernel_out
-//    for (int l = 0; l < channel_inputs; l++) {
-//        for (int k = 0; k < numofkernel; k++) {
-//            for (int i = 0; i < frow; i++) {
-//                delete[] kernel_out[l][k][i];
-//            }
-//            delete[] kernel_out[l][k];
-//        }
-//        delete[] kernel_out[l];
-//    }
-//    delete[] kernel_out;
-//}
-
 void main() {
     float* input= (float*)malloc((32*32*3) * sizeof(float));
 	int n_input;
